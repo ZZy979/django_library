e6f78ef7c481a4abb93eda3fca33b9e558ffe9bb
ZZy979 <979481894@qq.com>
2025-02-28 23:40:14 +0800

    支持区分用户角色，增加加载用户组和权限的命令

---
+++ library/management/commands/loadgroupperms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import Group, Permission
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Load library user groups and permissions'
+
+    def handle(self, *args, **options):
+        # create groups
+        librarian_group, _ = Group.objects.get_or_create(name='Librarian')
+        self.stdout.write('Created Librarian group')
+
+        # add permissions
+        perm_names = [
+            'add_book', 'change_book', 'delete_book', 'view_book',
+            'add_category', 'change_category', 'delete_category', 'view_category'
+        ]
+        librarian_perms = Permission.objects.filter(codename__in=perm_names)
+        librarian_group.permissions.set(librarian_perms)
+        self.stdout.write('Set Librarian group permissions to {}'.format(librarian_perms))
